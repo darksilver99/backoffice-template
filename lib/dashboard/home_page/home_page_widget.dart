bbf84a5460665152b9910ab20597741d1359d05e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-12 04:24:06 +0000

    Updating to latest FlutterFlow output.

--- lib/dashboard/home_page/home_page_widget.dart
+++ lib/dashboard/home_page/home_page_widget.dart
@@ -1,3 +1,4 @@
+import '/auth/custom_auth/auth_util.dart';
 import '/components/menu_button_view_widget.dart';
 import '/components/menu_view_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
@@ -104,9 +105,56 @@ class _HomePageWidgetState extends State<HomePageWidget> {
         ),
         body: SafeArea(
           top: true,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
+          child: Stack(
             children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0.0, 65.0, 0.0, 0.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text(
+                      'first_name : ${valueOrDefault<String>(
+                        currentUserData?.firstName,
+                        '-',
+                      )}',
+                      style: FlutterFlowTheme.of(context).bodyMedium.override(
+                            fontFamily: 'Readex Pro',
+                            letterSpacing: 0.0,
+                          ),
+                    ),
+                    FFButtonWidget(
+                      onPressed: () async {
+                        GoRouter.of(context).prepareAuthEvent();
+                        await authManager.signOut();
+                        GoRouter.of(context).clearRedirectLocation();
+
+                        context.goNamedAuth('LoginPage', context.mounted);
+                      },
+                      text: 'Button',
+                      options: FFButtonOptions(
+                        height: 40.0,
+                        padding: EdgeInsetsDirectional.fromSTEB(
+                            24.0, 0.0, 24.0, 0.0),
+                        iconPadding:
+                            EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                        color: FlutterFlowTheme.of(context).error,
+                        textStyle:
+                            FlutterFlowTheme.of(context).titleSmall.override(
+                                  fontFamily: 'Readex Pro',
+                                  color: Colors.white,
+                                  letterSpacing: 0.0,
+                                ),
+                        elevation: 3.0,
+                        borderSide: BorderSide(
+                          color: Colors.transparent,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               wrapWithModel(
                 model: _model.menuButtonViewModel,
                 updateCallback: () => setState(() {}),
